501a66730e4817f2cc9d0ae6bef68dcf437d0c6b
Noor_Rajab <rajabnoor115@gmail.com>
2025-11-09 01:22:25 +0300

    add decorator lib

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,7 @@ from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 
 # CORRECTION APPLIED: Explicitly importing permission_required
-from django.contrib.auth.decorators import user_passes_test, permission_required 
+from django.contrib.auth.decorators import permission_required, user_passes_test
 from django.contrib.auth.views import LoginView, LogoutView 
 from django.contrib.auth.forms import UserCreationForm 
 from django.contrib.auth import login
