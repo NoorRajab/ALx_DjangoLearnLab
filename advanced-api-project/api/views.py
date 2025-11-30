aa802729f7e4d37b0fdd9da8d9fc7512ce2a2dbf
Noor_Rajab <rajabnoor115@gmail.com>
2025-11-30 18:38:34 +0300

    filters2

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,4 @@
-# api/views.py
+from django_filters import rest_framework
 from rest_framework import generics, permissions
 from rest_framework import filters  # Import the filters module
 from django_filters.rest_framework import DjangoFilterBackend # Import the DjangoFilterBackend
